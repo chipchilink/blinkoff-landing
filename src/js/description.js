2ed65561ae46d9008f7d6cfcaf61e181e4781552
exeleNt <sbashmakov@skytracking.ru>
2024-02-25 00:17:26 +0400

    1%

--- src/js/description.js
+++ src/js/description.js
@@ -135,31 +135,27 @@ const monitoring = (tl) => {
     .add(noop, '+=0.01')
     .addLabel(l1)
     .add(onStart, l1)
-    .from('#descr1-smartphone-label', { x: '100%' }, l1)
+    .from('#descr1-smartphone-label', { x: '101%' }, l1)
     .from('#descr1-smartphone-img', { x: -d, opacity: 0, scale }, l1)
     .addLabel(l2)
-    .from('#descr1-controller-label', { x: '100%' }, l2)
+    .from('#descr1-controller-label', { x: '101%' }, l2)
     .from('#descr1-controller-img', { y: -d, opacity: 0, scale }, l2)
-    .addLabel(l3)
-    .from('#descr1-bluetooth', { opacity: 0 })
+    .from('#descr1-bluetooth', { opacity: 0 }, l2 + '+=0.2')
     .addLabel(l4)
-    .from('#descr1-panel-controller-label', { x: '100%' }, l4)
+    .from('#descr1-panel-controller-label', { x: '101%' }, l4)
     .from('#descr1-panel-controller-img', { x: d, opacity: 0, scale }, l4)
-    .addLabel(l5)
-    .from('#descr1-wifi', { opacity: 0 })
-    .addLabel(l6, '+=1')
-    .to('#descr1-smartphone-label', { x: '100%' }, l6)
+    .from('#descr1-wifi', { opacity: 0 }, l4 + '+=0.2')
+    .addLabel(l6)
+    .to('#descr1-smartphone-label', { x: '101%' }, l6)
     .to('#descr1-smartphone-img', { x: -d, opacity: 0, scale }, l6)
-    .to('#descr1-controller-label', { x: '100%' }, l6)
+    .to('#descr1-controller-label', { x: '101%' }, l6)
     .to('#descr1-controller-img', { y: -d, opacity: 0, scale }, l6)
-    .to('#descr1-panel-controller-label', { x: '100%' }, l6)
+    .to('#descr1-panel-controller-label', { x: '101%' }, l6)
     .to('#descr1-panel-controller-img', { x: d, opacity: 0, scale }, l6)
     .to('#descr1-bluetooth', { opacity: 0 }, l6)
     .to('#descr1-wifi', { opacity: 0 }, l6)
-    .addLabel(l7)
-    .add(onEnd, l7);
+    .add(onEnd);
 };
-
 const controller = (tl) => {
   const id = '#controller';
 
@@ -191,21 +187,19 @@ const controller = (tl) => {
   const d = 30;
 
   return tl
-    .addLabel(l1)
-    .add(onStart, l1)
+    .add(onStart)
     .set('#descr2-parent-plata-img', { width: 503 })
-    .from('#descr2-slave-plata-label', { x: '100%' }, l1)
-    .from('#descr2-slave-plata-img', { x: -d, opacity: 0, scale }, l1)
-    .from('#descr2-parent-plata-label', { x: '100%' }, l1)
-    .from('#descr2-parent-plata-img', { x: -d, opacity: 0, scale }, l1)
-    .addLabel(l2)
-    .addLabel(l3, '+=1')
-    .to('#descr2-slave-plata-label', { x: '100%' }, l3)
+    .from('#descr2-slave-plata-label', { x: '101%' })
+    .from('#descr2-slave-plata-img', { x: -d, opacity: 0, scale }, '<')
+    .from('#descr2-parent-plata-label', { x: '101%' }, '<')
+    .from('#descr2-parent-plata-img', { x: -d, opacity: 0, scale }, '<')
+    .addLabel(l3)
+    .to('#descr2-slave-plata-label', { x: '101%' }, l3)
     .to('#descr2-slave-plata-img', { x: -d, opacity: 0, scale }, l3)
-    .to('#descr2-parent-plata-label', { x: '100%' }, l3)
+    .to('#descr2-parent-plata-label', { x: '101%' }, l3)
     .to('#descr2-parent-plata-img', { scale: 1.5, y: -200 }, l3)
-    .addLabel(l4)
-    .add(onEnd, l4);
+    .add(onEnd)
+    .add(noop, '+=0.01');
 };
 
 const specifications = (tl) => {
@@ -230,20 +224,16 @@ const specifications = (tl) => {
   const d = 30;
 
   return tl
-    .addLabel(l1)
-    .add(linkActivate.in, l1)
-    .add(enter, l1)
-    .addLabel(l2)
-    .from('#tv-controller .point', { stagger: 0.1, scale: 0, y: 10 }, l2)
-    .from(id + ' .-a-list li', { stagger: 0.1, opacity: 0, x: 40 }, l2)
-    .addLabel(l3)
-    .addLabel(l4, '+=1')
+    .add(linkActivate.in)
+    .add(enter)
+    .from('#tv-controller .point', { stagger: 0.1, scale: 0, y: 10 },)
+    .from(id + ' .-a-list li', { stagger: 0.1, opacity: 0, x: 40 },)
+    .addLabel(l4)
     .to('#tv-controller .point', { stagger: 0.1, scale: 0, y: 10 }, l4)
-    .addLabel(l5)
-    .to('#descr2-parent-plata-img', { x: -d, opacity: 0, scale }, l5)
-    .addLabel(l6)
-    .add(leave, l6)
-    .add(linkActivate.out, l6);
+    .to('#descr2-parent-plata-img', { x: -d, opacity: 0, scale })
+    .add(leave)
+    .add(linkActivate.out)
+    .add(noop, '+=0.01');
 };
 
 const androidBase = (tl) => {
@@ -266,17 +256,15 @@ const androidBase = (tl) => {
   const d = 30;
 
   return tl
-    .addLabel(l1)
-    .add(linkActivate.in, l1)
-    .add(enter, l1)
-    .from('#descr4-iphone-app-img', { x: -d, opacity: 0, scale }, l1)
-    .addLabel(l2)
-    .addLabel(l3, '+=1')
-    .to('#descr4-iphone-app-img', { x: -d, opacity: 0, scale }, l3)
-    .addLabel(l4)
-    .add(leave, l4)
-    .add(linkActivate.out, l4)
-    .add(noop, '+=0.01');
+    .add(linkActivate.in)
+    .add(enter)
+    .from('#descr4-iphone-app-img', { x: -d, opacity: 0, scale })
+    .addLabel(l3)
+    // .to('#descr4-iphone-app-img', { x: -d, opacity: 0, scale })
+    // .add(leave)
+    .add(linkActivate.out)
+    .add(noop, '+=0.01')
+    .addLabel(l4);
 };
 
 // -------------------------------------------------------------------------------------
@@ -291,6 +279,7 @@ const t = pipe(
       start: 'top top',
       end: '+=5000',
       scrub: true,
+      snap: 'labelsDirectional'
     },
     onComplete: () => {
       scrolledTo('#complect');
